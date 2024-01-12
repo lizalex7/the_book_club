2cf53342768fad203008dbae3720577038de4e05
lizalex7 <lizmannis7@gmail.com>
2024-01-12 15:32:18 +0000

    completed full crud capability for books

--- books/views.py
+++ books/views.py
@@ -1,13 +1,15 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from .models import Book
 from .serializers.common import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers.populated import BookListSerializer
+from lib.views import OwnerListCreateView
+from lib.permissions import IsOwnerOrReadOnly
 
 
 # Path: /books/
 # Methods: GET, POST
-class BookListCreateView(ListCreateAPIView):
+class BookListCreateView(OwnerListCreateView):
   queryset = Book.objects.all()
   permission_classes = [IsAuthenticatedOrReadOnly]
 
@@ -15,3 +17,16 @@ class BookListCreateView(ListCreateAPIView):
     if self.request.method == 'GET':
       return BookListSerializer
     return BookSerializer
+  
+# Path: /books/:pk
+# Methods: GET, PUT, PATCH, DELETE
+
+class BookDetailView(RetrieveUpdateDestroyAPIView):
+  queryset = Book.objects.all()
+  permission_classes = [IsOwnerOrReadOnly]
+    
+  def get_serializer_class(self):
+    if self.request.method == 'GET':
+      return BookListSerializer
+    else:
+      return BookSerializer
---
+++ lib/views.py
@@ -0,0 +1,10 @@
+from rest_framework.generics import ListCreateAPIView
+
+class OwnerListCreateView(ListCreateAPIView):
+
+  def perform_create(self, serializer):
+    serializer.save(owner=self.request.user)
+
+
+    
+  
